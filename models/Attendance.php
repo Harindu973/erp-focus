785434e79bd1259041b37f6e9f4f4e6ec532a0d4
Harindu Lakshan <harindulakshankob@gmail.com>
2021-04-26 13:46:56 +0530

    update full page

--- models/Attendance.php
+++ models/Attendance.php
@@ -68,38 +68,38 @@
   //         $this->category_name = $row['category_name'];
   //   }
 
-  //   // Create Post
-  //   public function create() {
-  //         // Create query
-  //         $query = 'INSERT INTO ' . $this->table . ' SET title = :title, body = :body, author = :author, category_id = :category_id';
+    // Create Post
+    public function create() {
+          // Create query
+          $query = 'INSERT INTO ' . $this->table . ' SET title = :title, body = :body, author = :author, category_id = :category_id';
 
-  //         // Prepare statement
-  //         $stmt = $this->conn->prepare($query);
+          // Prepare statement
+          $stmt = $this->conn->prepare($query);
 
-  //         // Clean data
-  //         $this->title = htmlspecialchars(strip_tags($this->title));
-  //         $this->body = htmlspecialchars(strip_tags($this->body));
-  //         $this->author = htmlspecialchars(strip_tags($this->author));
-  //         $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+          // Clean data
+          $this->title = htmlspecialchars(strip_tags($this->title));
+          $this->body = htmlspecialchars(strip_tags($this->body));
+          $this->author = htmlspecialchars(strip_tags($this->author));
+          $this->category_id = htmlspecialchars(strip_tags($this->category_id));
 
-  //         // Bind data
-  //         $stmt->bindParam(':title', $this->title);
-  //         $stmt->bindParam(':body', $this->body);
-  //         $stmt->bindParam(':author', $this->author);
-  //         $stmt->bindParam(':category_id', $this->category_id);
+          // Bind data
+          $stmt->bindParam(':title', $this->title);
+          $stmt->bindParam(':body', $this->body);
+          $stmt->bindParam(':author', $this->author);
+          $stmt->bindParam(':category_id', $this->category_id);
 
-  //         // Execute query
-  //         if($stmt->execute()) {
-  //           return true;
-  //     }
+          // Execute query
+          if($stmt->execute()) {
+            return true;
+      }
 
-  //     // Print error if something goes wrong
-  //     printf("Error: %s.\n", $stmt->error);
+      // Print error if something goes wrong
+      printf("Error: %s.\n", $stmt->error);
 
-  //     return false;
-  //   }
+      return false;
+    }
 
- // Update Post
+    // Update Post
      public function update() {
           // Create query
           $query = 'UPDATE ' . $this->table . ' SET `attendance` = 1 WHERE `empId` = :empId';
@@ -124,29 +124,29 @@
       return false;
      }
 
-  //   // Delete Post
-  //   public function delete() {
-  //         // Create query
-  //         $query = 'DELETE FROM ' . $this->table . ' WHERE id = :id';
+    // // Delete Post
+    // public function delete() {
+    //       // Create query
+    //       $query = 'DELETE FROM ' . $this->table . ' WHERE id = :id';
 
-  //         // Prepare statement
-  //         $stmt = $this->conn->prepare($query);
+    //       // Prepare statement
+    //       $stmt = $this->conn->prepare($query);
 
-  //         // Clean data
-  //         $this->id = htmlspecialchars(strip_tags($this->id));
+    //       // Clean data
+    //       $this->id = htmlspecialchars(strip_tags($this->id));
 
-  //         // Bind data
-  //         $stmt->bindParam(':id', $this->id);
+    //       // Bind data
+    //       $stmt->bindParam(':id', $this->id);
 
-  //         // Execute query
-  //         if($stmt->execute()) {
-  //           return true;
-  //         }
+    //       // Execute query
+    //       if($stmt->execute()) {
+    //         return true;
+    //       }
 
-  //         // Print error if something goes wrong
-  //         printf("Error: %s.\n", $stmt->error);
+    //       // Print error if something goes wrong
+    //       printf("Error: %s.\n", $stmt->error);
 
-  //         return false;
-  //   }
+    //       return false;
+    // }
     
  }
