eea886e780754995c56c6296cea0487f9bfb4688
Harindu973 <harindulakshankob@gmail.com>
2021-05-29 12:56:12 +0530

    Hi file test added

---
+++ api/employee/hi.php
@@ -0,0 +1,5 @@
+<html>
+  <body>
+    <h1>Hellow I'm Running. Hi.</h1>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
   <body>
     <h1>Hellow I'm Running. Get my endpoints.</h1>
+    <a href="./api/employee/hi.php">click me</a>
   </body>
 </html>
