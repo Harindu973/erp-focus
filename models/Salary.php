f115730ce794ad7289753e2917c7a8e863017a6f
Harindu973 <harindulakshankob@gmail.com>
2021-05-29 21:53:16 +0530

    added read filter

--- models/Salary.php
+++ models/Salary.php
@@ -38,35 +38,47 @@
       return $stmt;
     }
 
-  //   // Get Single Post
-  //   public function read_single() {
-  //         // Create query
-  //         $query = 'SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at
-  //                                   FROM ' . $this->table . ' p
-  //                                   LEFT JOIN
-  //                                     categories c ON p.category_id = c.id
-  //                                   WHERE
-  //                                     p.id = ?
-  //                                   LIMIT 0,1';
+     // Get Posts
+     public function read_filter() {
+      // Create query
 
-  //         // Prepare statement
-  //         $stmt = $this->conn->prepare($query);
+      // $query = 'SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at
+      //                           FROM ' . $this->table . ' p
+      //                           LEFT JOIN
+      //                             categories c ON p.category_id = c.id
+      //                           ORDER BY
+      //                             p.created_at DESC';
 
-  //         // Bind ID
-  //         $stmt->bindParam(1, $this->id);
+      $query = 'SELECT * FROM ' . $this->table . ' WHERE empId = ?';
+      
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
 
-  //         // Execute query
-  //         $stmt->execute();
+      // Bind ID
+      $stmt->bindParam(1, $this->empId);
 
-  //         $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      // Execute query
+      $stmt->execute();
 
-  //         // Set properties
-  //         $this->title = $row['title'];
-  //         $this->body = $row['body'];
-  //         $this->author = $row['author'];
-  //         $this->category_id = $row['category_id'];
-  //         $this->category_name = $row['category_name'];
-  //   }
+      return $stmt;
+    }
+
+    // Get Single Post
+    public function read_single() {
+      // Create query
+      $query = 'SELECT * FROM ' . $this->table . ' WHERE empId = ?';
+
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
+
+      // Bind ID
+      $stmt->bindParam(1, $this->empId);
+
+      // Execute query
+      $stmt->execute();
+
+      return $stmt;
+     }
 
   //   // Create Post
     public function create() {
