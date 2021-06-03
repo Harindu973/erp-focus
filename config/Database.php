110948327eee300325430670f115941f31c93ca1
Harindu973 <harindulakshankob@gmail.com>
2021-06-03 12:24:44 +0530

    Changed to private ip

--- config/Database.php
+++ config/Database.php
@@ -2,8 +2,8 @@
 
   class Database {
      // DB Params
-    //private $host = '10.52.0.4';
-    private $host = '35.232.58.160';
+    private $host = '10.52.0.4';
+    //private $host = '35.232.58.160';
     private $db_name = 'heroku_e60a83519a7bbe7';
     private $username = 'harindu';
     private $password = 'H123456';
